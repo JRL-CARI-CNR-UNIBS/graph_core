3c0d67a991c0a78fd3775a9280af3774af67491a
Cesare Tonola <c.tonola001@unibs.it>
2022-07-28 14:33:57 +0200

    more logs

--- graph_core/include/graph_core/log.h
+++ graph_core/include/graph_core/log.h
@@ -43,18 +43,34 @@ namespace ros_log_color
     MAGENTA,
     CYAN,
     WHITE,
+    BOLDBLACK,
+    BOLDRED,
+    BOLDGREEN,
+    BOLDYELLOW,
+    BOLDBLUE,
+    BOLDMAGENTA,
+    BOLDCYAN,
+    BOLDWHITE,
     ENDCOLOR
   };
 
   std::ostream& operator<<(std::ostream& os, PRINT_COLOR c);
 }
 
-#define ROS_BLACK_STREAM(x)   ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BLACK   << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_RED_STREAM(x)     ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::RED     << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_GREEN_STREAM(x)   ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::GREEN   << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_YELLOW_STREAM(x)  ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::YELLOW  << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_BLUE_STREAM(x)    ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BLUE    << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_MAGENTA_STREAM(x) ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::MAGENTA << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_CYAN_STREAM(x)    ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::CYAN    << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
-#define ROS_WHITE_STREAM(x)   ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::WHITE   << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BLACK_STREAM(x)       ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BLACK       << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_RED_STREAM(x)         ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::RED         << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_GREEN_STREAM(x)       ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::GREEN       << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_YELLOW_STREAM(x)      ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::YELLOW      << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BLUE_STREAM(x)        ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BLUE        << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_MAGENTA_STREAM(x)     ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::MAGENTA     << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_CYAN_STREAM(x)        ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::CYAN        << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_WHITE_STREAM(x)       ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::WHITE       << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDBLACK_STREAM(x)   ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDBLACK   << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDRED_STREAM(x)     ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDRED     << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDGREEN_STREAM(x)   ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDGREEN   << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDYELLOW_STREAM(x)  ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDYELLOW  << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDBLUE_STREAM(x)    ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDBLUE    << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDMAGENTA_STREAM(x) ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDMAGENTA << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDCYAN_STREAM(x)    ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDCYAN    << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
+#define ROS_BOLDWHITE_STREAM(x)   ROS_INFO_STREAM(ros_log_color::PRINT_COLOR::BOLDWHITE   << x << ros_log_color::PRINT_COLOR::ENDCOLOR)
 
--- graph_core/src/graph_core/log.cpp
+++ graph_core/src/graph_core/log.cpp
@@ -32,16 +32,24 @@ std::ostream& operator<<(std::ostream& os, PRINT_COLOR c)
 {
   switch(c)
   {
-  case BLACK    : os << "\033[1;30m"; break;
-  case RED      : os << "\033[1;31m"; break;
-  case GREEN    : os << "\033[1;32m"; break;
-  case YELLOW   : os << "\033[1;33m"; break;
-  case BLUE     : os << "\033[1;34m"; break;
-  case MAGENTA  : os << "\033[1;35m"; break;
-  case CYAN     : os << "\033[1;36m"; break;
-  case WHITE    : os << "\033[1;37m"; break;
-  case ENDCOLOR : os << "\033[0m";    break;
-  default       : os << "\033[1;37m";
+  case BLACK       : os << "\033[30m"  ; break;
+  case RED         : os << "\033[31m"  ; break;
+  case GREEN       : os << "\033[32m"  ; break;
+  case YELLOW      : os << "\033[33m"  ; break;
+  case BLUE        : os << "\033[34m"  ; break;
+  case MAGENTA     : os << "\033[35m"  ; break;
+  case CYAN        : os << "\033[36m"  ; break;
+  case WHITE       : os << "\033[37m"  ; break;
+  case BOLDBLACK   : os << "\033[1;30m"; break;
+  case BOLDRED     : os << "\033[1;31m"; break;
+  case BOLDGREEN   : os << "\033[1;32m"; break;
+  case BOLDYELLOW  : os << "\033[1;33m"; break;
+  case BOLDBLUE    : os << "\033[1;34m"; break;
+  case BOLDMAGENTA : os << "\033[1;35m"; break;
+  case BOLDCYAN    : os << "\033[1;36m"; break;
+  case BOLDWHITE   : os << "\033[1;37m"; break;
+  case ENDCOLOR    : os << "\033[0m";    break;
+  default          : os << "\033[37m";
   }
   return os;
 }
