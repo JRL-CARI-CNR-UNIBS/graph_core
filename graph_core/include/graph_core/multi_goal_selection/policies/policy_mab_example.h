6f0a8a8e850dbf5889feaffa1843ad34f8fbea21
Marco Faroni <marco.faroni@stiima.cnr.it>
2022-10-05 12:31:55 -0400

    mfaroni : changed MAB example

--- graph_core/include/graph_core/multi_goal_selection/policies/policy_mab_example.h
+++ graph_core/include/graph_core/multi_goal_selection/policies/policy_mab_example.h
@@ -37,20 +37,38 @@ class PolicyMABExample: public PolicyMAB
 public:
   PolicyMABExample(const std::string& name, const int& n_goals) : PolicyMAB(name, n_goals){}
 
+  /* Dummy MAB that select arm with probability weighted by reward^2 of each arm */
   int selectNextArm()
   {
-//    ROS_INFO("select next arm example");
-    return 0;
-  };
+    int iarm = std::uniform_int_distribution<int>(0,n_goals_-1)(gen_);
+    double sum=0;
+    for (unsigned int idx=0;idx<n_goals_;idx++)
+      sum += expected_reward_[idx]*expected_reward_[idx];
+
+    double rnd = std::uniform_real_distribution<double>(0.0,sum)(gen_);
+    for(int idx=0; idx<n_goals_; idx++)
+    {
+      if(rnd < expected_reward_[idx]*expected_reward_[idx])
+        return iarm;
+      rnd -= expected_reward_[idx]*expected_reward_[idx];
+    }
+    return iarm;
+  }
+
+  /* Dummy MAB that updates the reward as average of past rewards*/
   void updateState(const int& i_goal, const double& reward)
   {
-//    ROS_INFO("update state example");
-  };
+    expected_reward_[i_goal] *=  pull_counter_[i_goal];
+    expected_reward_[i_goal] +=  reward;
+    pull_counter_[i_goal]++;
+    expected_reward_[i_goal] /=  pull_counter_[i_goal];
+  }
+
   std::string toString()
   {
     std::string str = "example";
     return str;
-  };
+  }
 
 };
 typedef std::shared_ptr<PolicyMABExample> PolicyMABExamplePtr;
