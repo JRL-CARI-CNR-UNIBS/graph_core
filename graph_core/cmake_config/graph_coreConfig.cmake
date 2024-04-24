43440a7a8e356337c79aedc882376a3ea6dfb652
Manuel Beschi <manuel.beschi@unibs.it>
2024-04-24 17:20:13 +0200

    update to cnr_param with targets

--- graph_core/cmake_config/graph_coreConfig.cmake
+++ graph_core/cmake_config/graph_coreConfig.cmake
@@ -1,10 +1,10 @@
 include(CMakeFindDependencyMacro)
 
+find_dependency(cnr_param REQUIRED)
 find_dependency(Eigen3 REQUIRED COMPONENTS core)
 find_dependency(Boost REQUIRED COMPONENTS date_time filesystem)
-find_dependency(PkgConfig REQUIRED)
+find_package(PkgConfig REQUIRED)
 pkg_check_modules(YAML_CPP REQUIRED yaml-cpp IMPORTED_TARGET)
 
 find_package(cnr_logger REQUIRED)
-find_package(cnr_param REQUIRED)
 include("${CMAKE_CURRENT_LIST_DIR}/graph_coreTargets.cmake")
