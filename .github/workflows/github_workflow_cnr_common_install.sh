60aa7fe7328d79515d2c52ddf5c8dabca7567d38
Cesare Tonola <c.tonola001@unibs.it>
2024-11-04 18:24:32 +0100

    workflow
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow
    
    fix workflow

---
+++ .github/workflows/github_workflow_cnr_common_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir graph_core_ws
+cd graph_core_ws
+export PATH_TO_WS="$(pwd)"
+
+echo "PATH_TO_WS=$PATH_TO_WS" >> $GITHUB_ENV
+echo "Workspace Path: $PATH_TO_WS"
+
+#Install cnr_common
+mkdir -p $PATH_TO_WS/src
+cd $PATH_TO_WS/src
+git clone --recurse-submodules https://github.com/JRL-CARI-CNR-UNIBS/cnr_common.git
+
+cd $PATH_TO_WS/src/cnr_common
+git submodule update --init --recursive
+. update_submodules.sh
+
+cd $PATH_TO_WS
+cp src/cnr_common/build_template.sh build.sh
+chmod +x build.sh
+. build.sh
+
+#Export path to lib
+echo "PATH=$PATH_TO_WS/install/bin:$PATH" >> $GITHUB_ENV
+echo "LD_LIBRARY_PATH=$PATH_TO_WS/install/lib" >> $GITHUB_ENV
+echo "CMAKE_PREFIX_PATH=$PATH_TO_WS/install" >> $GITHUB_ENV
+
+
--- .github/workflows/github_workflow_graph_core_install.sh
+++ .github/workflows/github_workflow_graph_core_install.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-
-export PATH_TO_WS="$(pwd)"
-
-#Install cnr_common
-mkdir -p $PATH_TO_WS/src
-cd $PATH_TO_WS/src
-git clone --recurse-submodules https://github.com/JRL-CARI-CNR-UNIBS/cnr_common.git
-
-cd $PATH_TO_WS/src/cnr_common
-git submodule update --init --recursive
-. update_submodules.sh
-
+echo "Workspace Path: $PATH_TO_WS"
 cd $PATH_TO_WS
-cp src/cnr_common/build_template.sh build.sh && sed -i '1i PATH_TO_WS="$(PATH_TO_WS)"' build.sh
-. build.sh
 
+echo "Install Folder: $PATH_TO_WS/install"
+cd $PATH_TO_WS/install
+echo "PATH: $PATH"
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
+echo "CMAKE_PREFIX_PATH: $CMAKE_PREFIX_PATH"
 
-#Install graph_core
+echo "Files in $PATH_TO_WS/install:"
+ls -R $PATH_TO_WS/install
+
+# Install graph_core
+cd $PATH_TO_WS/src
+git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git
+
 cd $PATH_TO_WS
 mkdir -p build/graph_core
 cmake -S src/graph_core/graph_core -B build/graph_core -DCMAKE_INSTALL_PREFIX=$PATH_TO_WS/install
 make -C build/graph_core install
-
