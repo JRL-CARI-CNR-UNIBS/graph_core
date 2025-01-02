9935190f50712fb89f58f3533c98ea8fea3ba690
Cesare Tonola <c.tonola001@unibs.it>
2025-01-02 18:49:05 +0100

    fixing for catkin

--- graph_core/cmake_config/Dependencies.cmake
+++ graph_core/cmake_config/Dependencies.cmake
@@ -20,13 +20,42 @@ CPMFindPackage(
 
 message("\n-----------------------------------------------\n")
 
-CPMFindPackage(
-  NAME cnr_yaml
-  GITHUB_REPOSITORY CNR-STIIMA-IRAS/cnr_yaml
-  GIT_TAG master
-  OPTIONS
-    "BUILD_UNIT_TESTS OFF"
-)
+# If cnr_param is not already installed on the system, install cnr_yaml before cnr_param.
+# cnr_yaml could be already installed or missing. If it is missing and ROS1 is sourced,
+# fix the installation path of cnr_yaml from install/share/cnr_yaml/cmake_alternative to
+# install/share/cnr_yaml/cmake (see cnr_yaml cmake configuration files).
+
+string(ASCII 27 Esc)
+set(RESET "${Esc}[m")
+set(BLUE "${Esc}[34m")
+
+find_package(cnr_param QUIET)
+
+if(NOT cnr_param_FOUND)
+  # Check if cnr_yaml is already installed on the system
+  find_package(cnr_yaml QUIET)
+
+  if(NOT cnr_yaml_FOUND)
+    # If cnr_yaml is not found, use CPM to install it
+    CPMAddPackage(
+      NAME cnr_yaml
+      GITHUB_REPOSITORY CNR-STIIMA-IRAS/cnr_yaml
+      GIT_TAG master
+      OPTIONS
+        "BUILD_UNIT_TESTS OFF"
+    )
+
+    # Attempt to detect Catkin
+    find_package(catkin QUIET)
+    if(catkin_FOUND)
+      message(STATUS "${BLUE}Catkin found.${RESET}")
+      set(CLONE_CNR_YAML_CMAKE_ALTERNATIVE ON)
+    else()
+      message(STATUS "${BLUE}Catkin not found.${RESET}")
+    endif()
+    message(STATUS "${BLUE}CLONE_CNR_YAML_CMAKE_ALTERNATIVE=${CLONE_CNR_YAML_CMAKE_ALTERNATIVE}${RESET}")
+  endif()
+endif()
 
 message("\n-----------------------------------------------\n")
 
