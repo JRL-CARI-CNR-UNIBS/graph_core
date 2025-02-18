16231a974dc631eba01c5e8ce5ef96993c7c4724
Cesare Tonola <c.tonola001@unibs.it>
2025-02-18 10:27:59 +0100

    more fix

--- .github/workflows/github_workflow_graph_core_install_within_catkin.sh
+++ .github/workflows/github_workflow_graph_core_install_within_catkin.sh
@@ -14,11 +14,12 @@ echo "Setting up Catkin workspace at $WORKSPACE_DIR"
 mkdir -p $SRC_DIR
 cd $WORKSPACE_DIR
 
-# Clone graph_core into the src folder if not already present
-if [ ! -d "$SRC_DIR/graph_core" ]; then
-    echo "Cloning graph_core repository into $SRC_DIR"
-    git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git $SRC_DIR/graph_core
-fi
+# Clone graph_core into the src folder
+echo "Cloning graph_core repository into $SRC_DIR"
+git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git $SRC_DIR/graph_core
+
+echo "Cloning deps with vcstool into $SRC_DIR"
+vcs import < graph_core/deps.repos
 
 # Build the workspace
 echo "Building the Catkin workspace"
