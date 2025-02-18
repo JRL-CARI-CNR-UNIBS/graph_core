995be9905ef02d2bd9d56d116269f4c14a1f722f
Cesare Tonola <c.tonola001@unibs.it>
2025-02-18 11:18:45 +0100

    rearranging workflows

--- .github/workflows/github_workflow_graph_core_install_within_catkin.sh
+++ .github/workflows/github_workflow_graph_core_install_within_catkin.sh
@@ -12,18 +12,16 @@ SRC_DIR=$WORKSPACE_DIR/src
 # Create the workspace and source folder
 echo "Setting up Catkin workspace at $WORKSPACE_DIR"
 mkdir -p $SRC_DIR
-cd $WORKSPACE_DIR
 
 # Clone graph_core into the src folder
+cd $WORKSPACE_DIR
 echo "Cloning graph_core repository into $SRC_DIR"
 git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git $SRC_DIR/graph_core
-
-echo "Cloning deps with vcstool into $SRC_DIR"
-vcs import < graph_core/deps.repos
 
 # Build the workspace
 echo "Building the Catkin workspace"
 catkin config --extend /opt/ros/noetic
+catkin config --install
 catkin build -cs --cmake-args -DUSE_ROS1=OFF
 
 # Source the workspace setup
--- .github/workflows/github_workflow_graph_core_install_within_catkin_vcstool.sh
+++ .github/workflows/github_workflow_graph_core_install_within_catkin_vcstool.sh
@@ -15,28 +15,20 @@ mkdir -p $SRC_DIR
 
 # Clone graph_core into the src folder if not already present
 cd $WORKSPACE_DIR
-if [ ! -d "$SRC_DIR/graph_core" ]; then
-    echo "Cloning graph_core repository into $SRC_DIR"
-    git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git $SRC_DIR/graph_core
-fi
+echo "Cloning graph_core repository into $SRC_DIR"
+git clone https://github.com/JRL-CARI-CNR-UNIBS/graph_core.git $SRC_DIR/graph_core
 
 # Download deps
 cd $SRC_DIR
 vcs import < graph_core/deps.repos
 
-# Check for missing catkin package
-cd $WORKSPACE_DIR
-if [ ! -d "$SRC_DIR/catkin" ]; then
-    echo "Cloning catkin package into $SRC_DIR"
-    git clone https://github.com/ros/catkin.git $SRC_DIR/catkin
-fi
-
 # Build the workspace
 cd $WORKSPACE_DIR
 echo "Building the Catkin workspace"
 catkin config --extend /opt/ros/noetic
+catkin config --install
 catkin build --cmake-args -DUSE_ROS1=OFF
 
 # Source the workspace setup
 echo "Sourcing workspace setup"
-source $WORKSPACE_DIR/devel/setup.bash
+source $WORKSPACE_DIR/install/setup.bash
